669b4f4fba500d7b2c9542d16bf92f70db6e9019
teacher <teacher@4PC0>
2023-12-27 20:47:37 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/Phone.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Phone : INotifyPropertyChanged
+    {
+        private string _title;
+        private string _model;
+        private int _price;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        public string Model { 
+            get 
+            { 
+                return _model;
+            }
+            set 
+            { 
+                _model = value;
+                OnPropertyChanged("Model");
+            } 
+        }
+
+        public int Price {
+            get { return _price; }
+            set
+            { 
+                _price = value;
+                OnPropertyChanged("Price");
+            } 
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null) 
+                PropertyChanged(this, new PropertyChangedEventArgs(prop)); 
+        }
+    }
+}
---
+++ WpfApp1/PhoneViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class PhoneViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ?? (addCommand = new RelayCommand(
+                    obj =>
+                    {
+                        Phone phone = new Phone();
+                        Phones.Insert(0, phone);
+                        SelectedPhone = phone;
+                    }));
+            }
+        }
+
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public PhoneViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone{Title = "Iphone", Model = "X", Price = 100},
+                new Phone{Title = "Samsung", Model = "S23", Price = 1000},
+                new Phone{Title = "Xiaomi", Model = "12T", Price = 500}
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
